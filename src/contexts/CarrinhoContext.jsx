4ecd8bed33348e4b1e5930c519b2cd1457dfc11b
ledsouza <leandro.souza.159@gmail.com>
2024-07-22 13:54:06 -0300

    refatorando código

--- src/contexts/CarrinhoContext.jsx
+++ src/contexts/CarrinhoContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-export const CarrinhoContext = createContext;
+export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({ children }) => {
     const [carrinho, setCarrinho] = useState([]);
