b30e4681544ff22253fc98eca351f69844eb2c3a
ledsouza <leandro.souza.159@gmail.com>
2024-07-23 09:53:10 -0300

    implementando cache no carrinho

--- src/hooks/useCarrinhoContext.jsx
+++ src/hooks/useCarrinhoContext.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useMemo } from "react";
 import { CarrinhoContext } from "@/contexts/CarrinhoContext";
 
 export const useCarrinhoContext = () => {
@@ -45,8 +45,8 @@ export const useCarrinhoContext = () => {
         setCarrinho(produto);
     }
 
-    useEffect(() => {
-        const { totalTemp, quantidadeTemp } = carrinho.reduce(
+    const { totalTemp, quantidadeTemp } = useMemo(() => {
+        return carrinho.reduce(
             (acumulador, produto) => ({
                 quantidadeTemp: acumulador.quantidadeTemp + produto.quantidade,
                 totalTemp: acumulador.totalTemp + produto.preco * produto.quantidade,
@@ -56,10 +56,12 @@ export const useCarrinhoContext = () => {
                 totalTemp: 0,
             }
         );
+    }, [carrinho]);
 
+    useEffect(() => {
         setQuantidade(quantidadeTemp);
         setValorTotal(totalTemp);
-    }, [carrinho]);
+    });
 
     return {
         carrinho,
